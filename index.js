1c9f47d606ae09d9efcd1fe26d2f3c550766ea2f
anuragpampati <anuragpampati696@gmail.com>
2023-12-29 00:26:02 -0500

    working index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const port = 3000;
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
-  database: "world",
-  password: "123456",
+  database: "wold",
+  password: "0191",
   port: 5432,
 });
 
